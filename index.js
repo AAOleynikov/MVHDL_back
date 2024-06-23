6cf56fa8ce59dc696e0060be4160f6bb73616e30
Zhukov Georgiy <zhugeo@mail.ru>
2024-06-23 15:39:05 +0300

    Попытка подключить ts

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-import express from 'express';
-import { createServer } from 'node:http';
-
-const app = express();
-const server = createServer(app);
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+const a=undefined
+console.log(a??"undef")
