55ee21ddebe2e4677ab7b6db769eb457f7eef808
Zhukov Georgiy <zhugeo@mail.ru>
2024-06-24 09:37:54 +0300

    Удалён лишний файл

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { ValidationResult, validateProject, Project } from "./svhdl";
+import * as path from "path";
+import { ValidationResult, validateProject, Project, simulate } from "./svhdl";
+import * as fs from "fs";
+import { chdir } from "process";
+import { exec } from "child_process";
+import cors from "cors"
+
+
+exec(`rm -r run/*`);
+chdir("run")
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT;
+if (port === undefined) {
+  console.log("running default port");
+  port = "3000";
+}
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is working!");
@@ -22,16 +36,29 @@ function checkContentType(req, res, next) {
 
 app.post("/validate", checkContentType, (req: Request, res: Response) => {
   req.accepts("application/json");
-  const body: Project = req.body;
+  const body: Project = req.body.data;
   validateProject(body)
     .then((result) => {
-      res.statusCode = 200;
       res.contentType("application/json");
       res.send(result);
     })
-    .catch(() => {
-      res.statusCode = 500;
-      res.send("Server side error");
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Server side error");
+    });
+});
+
+app.post("/simulate", checkContentType, (req: Request, res: Response) => {
+  req.accepts("application/json");
+  const body: Project = req.body.data;
+  simulate(body)
+    .then((result) => {
+      res.contentType("application/json");
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Server side error");
     });
 });
 
